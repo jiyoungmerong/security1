425e56fe61940e176d05dfabbb36c137911452e7
jiyoungmerong <jiyoungmerong@gmail.com>
2023-06-27 02:23:00 +0900

    권한별 GetMapping과 로그인, 회원가입 api 추가

--- src/main/java/com/cos/security1/controller/IndexController.java
+++ src/main/java/com/cos/security1/controller/IndexController.java
@@ -1,15 +1,54 @@
 package com.cos.security1.controller;
 
+import com.cos.security1.model.User;
+import com.cos.security1.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class IndexController {
+    @Autowired
+    private UserRepository userRepository;
     @GetMapping({ "", "/" })
     public String index() {
-        // 머스테 이용. 기본 폴더 : src/main/resources/
+        // 머스테치 이용. 기본 폴더 : src/main/resources/
         // 뷰 리졸버 설정 : templates (prefix), .mustache (suffix)
         return "index";
+    }
+
+    @GetMapping("/user")
+    public @ResponseBody String user(){
+        return "user";
+    }
+
+    @GetMapping("/admin")
+    public @ResponseBody String admin(){
+        return "admin";
+    }
+
+    @GetMapping("/manager")
+    public @ResponseBody String manager(){
+        return "manager";
+    }
+
+    @GetMapping("/loginForm")
+    public String loginForm(){
+        return "loginForm";
+    }
+
+    @PostMapping("/join") // 실제 회원가입
+    public @ResponseBody String join(User user){
+        System.out.println(user);
+        user.setRole("ROLE_USER");
+        userRepository.save(user);
+        return "join";
+    }
+
+    @GetMapping("/joinForm") // 회원가입 폼
+    public String joinForm(){
+        return "joinForm";
     }
 }
