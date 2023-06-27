1830c46b4db2aeb0318d2c0b6984c26530af79b1
jiyoungmerong <jiyoungmerong@gmail.com>
2023-06-27 22:58:12 +0900

    IndexController 최종 수정. 권한별 컨트롤러 추가

--- src/main/java/com/cos/security1/controller/IndexController.java
+++ src/main/java/com/cos/security1/controller/IndexController.java
@@ -1,15 +1,81 @@
 package com.cos.security1.controller;
 
+import com.cos.security1.model.User;
+import com.cos.security1.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.annotation.Secured;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class IndexController {
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private BCryptPasswordEncoder bCryptPasswordEncoder;
+
     @GetMapping({ "", "/" })
     public String index() {
         // 머스테 이용. 기본 폴더 : src/main/resources/
         // 뷰 리졸버 설정 : templates (prefix), .mustache (suffix)
         return "index";
+    }
+
+    @GetMapping("/user")
+    public @ResponseBody  String user(){
+        return "user";
+    }
+
+    @GetMapping("/admin")
+    public @ResponseBody String admin(){
+        return "admin";
+    }
+
+    @GetMapping("/manager")
+    public @ResponseBody String manager(){
+        return "manager";
+    }
+
+    @GetMapping("/loginForm")
+    public String loginForm() {
+        return "loginForm";
+    }
+
+    @GetMapping("/login")
+    public String login(){
+        return "loginForm";
+    }
+
+    @GetMapping("/joinForm")
+    public String joinForm(){
+        return "joinForm";
+    }
+
+    @PostMapping("/join")
+    public String join(User user){
+        System.out.println(user);
+        user.setRole("ROLE_USER");
+        String rawPassword = user.getPassword();
+        String encPassword = bCryptPasswordEncoder.encode(rawPassword); // 비밀번호 암호화
+        user.setPassword(encPassword);
+
+        userRepository.save(user);
+        return "redirect:/loginForm";
+    }
+
+    @Secured("ROLE_ADMIN") // 하나의 권한만 필요할 때 사용
+    @GetMapping("/info")
+    public @ResponseBody String info(){
+        return "개인정보";
+    }
+    @PreAuthorize("hasRole('ROLE_MANAGER') or hasRole('ROLE_ADMIN')") // data메소드가 실행되기 직전에 실행됨 (2개 이상의 권한)
+    @GetMapping("/data")
+    public @ResponseBody String data(){
+        return "데이터 정보";
     }
 }
