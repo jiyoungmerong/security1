f7c08e8ddda1cade467417a3a189503ffb9e80c8
jiyoungmerong <jiyoungmerong@gmail.com>
2023-06-27 22:59:44 +0900

    SecurityConfig 필터 수정과 비밀번호 암호화 추가

--- src/main/java/com/cos/security1/config/SecurityConfig.java
+++ src/main/java/com/cos/security1/config/SecurityConfig.java
@@ -3,25 +3,38 @@ package com.cos.security1.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.authentication.configuration.EnableGlobalAuthentication;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration // IoC 빈(bean)을 등록
 @EnableWebSecurity // 필터 체인 관리 시작 어노테이션
+@EnableGlobalMethodSecurity(securedEnabled = true,  prePostEnabled = true) // secured 어노테이션 활성화, preAuthorize 어노테이션 활성화
 public class SecurityConfig {
+
+    @Bean // 해당 메소드의 리턴되는 오브젝트를 IoC로 등록해준다.
+    public BCryptPasswordEncoder encoderPwd(){
+        return new BCryptPasswordEncoder();
+    }
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http.csrf().disable();
-        http.authorizeHttpRequests()
+        http.csrf(csrf -> csrf.disable())
+                .authorizeRequests()
                 .requestMatchers("/user/**").authenticated()
                 .requestMatchers("/manager/**").hasAnyRole("MANAGER", "ADMIN")
                 .requestMatchers("/admin/**").hasRole("ADMIN")
                 .anyRequest().permitAll()
                 .and()
-                .formLogin()
-                .loginPage("/loginForm"); // 로그인 안 된 상태면 로그인 페이지로 이동하게 함
+                .formLogin(form -> form
+                        .loginPage("/loginForm")
+                        .loginProcessingUrl("/login")
+                        .defaultSuccessUrl("/")
+                        .permitAll());
 
         return http.build();
     }
